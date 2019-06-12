dca4d999c61a38962b54d10299782177a0a4b164
Andrew Burgess <andrew@andrewburgess.io>
2019-06-12 12:32:26 -0700

    :sparkles: feat: add seeding capabilities

--- js/app.js
+++ js/app.js
@@ -1,27 +1,103 @@
-function getIframe() {
-    return document.getElementById("site")
+class App {
+    constructor() {
+        this.$iframe = document.getElementById("site")
+        this.$next = document.getElementById("datscool")
+        this.$removeSeed = document.getElementById("remove-seed")
+        this.$addSeed = document.getElementById("seed")
+
+        this.$next.addEventListener("click", () => this.loadNextSite())
+        this.$removeSeed.addEventListener("click", () => this.removeSeed())
+        this.$addSeed.addEventListener("click", () => this.addSeed())
+
+        this.onSiteLoad = this.onSiteLoad.bind(this)
+
+        this.checkSeedCapability()
+        if (experimental) {
+            if (!experimental.library) {
+                document.removeChild(this.$addSeed)
+                document.removeChild(this.$removeSeed)
+            } else {
+                experimental.library
+                    .list({
+                        isSaved: true
+                    })
+                    .then((records) => {
+                        console.log(records)
+                    })
+            }
+        }
+    }
+
+    async checkSeedCapability() {
+        if (experimental && experimental.library) {
+            // Check if the user will let us read the library
+            try {
+                await experimental.library.list({ isSaved: true })
+                // If we make it here, we're good?
+                this.canSeed = true
+            } catch (e) {
+                // Assume user denied us
+                console.error(e)
+                document.removeChild(this.$addSeed)
+                document.removeChild(this.$removeSeed)
+                this.canSeed = false
+            }
+        } else {
+            document.removeChild(this.$addSeed)
+            document.removeChild(this.$removeSeed)
+
+            this.canSeed = false
+        }
+    }
+
+    loadNextSite() {
+        this.$iframe.classList.remove("loaded")
+        this.$iframe.addEventListener("load", this.onSiteLoad)
+
+        this.$iframe.src = "dat://pfrazee.hashbase.io/blog/unwalled-garden"
+
+        this.$addSeed.setAttribute("disabled", "disabled")
+        this.$removeSeed.setAttribute("disabled", "disabled")
+    }
+
+    async onSiteLoad() {
+        this.$iframe.removeEventListener("load", this.onSiteLoad)
+        this.$iframe.classList.add("loaded")
+
+        this.currentArchive = new DatArchive(this.$iframe.src)
+        this.currentArchiveInfo = await this.currentArchive.getInfo()
+
+        if (this.canSeed && experimental && experimental.library) {
+            const isSeeding = await experimental.library.get(this.currentArchiveInfo.url)
+
+            if (isSeeding.isSaved) {
+                this.$addSeed.classList.add("hidden")
+                this.$removeSeed.classList.remove("hidden")
+            } else {
+                this.$addSeed.classList.remove("hidden")
+                this.$removeSeed.classList.add("hidden")
+            }
+
+            this.$addSeed.removeAttribute("disabled")
+            this.$removeSeed.removeAttribute("disabled")
+        }
+    }
+
+    async removeSeed() {
+        const settings = await experimental.library.requestRemove(this.currentArchiveInfo.url)
+        if (!settings.isSaved) {
+            this.$addSeed.classList.remove("hidden")
+            this.$removeSeed.classList.add("hidden")
+        }
+    }
+
+    async addSeed() {
+        const settings = await experimental.library.requestAdd(this.currentArchiveInfo.url)
+        if (settings.isSaved) {
+            this.$addSeed.classList.add("hidden")
+            this.$removeSeed.classList.remove("hidden")
+        }
+    }
 }
 
-const onSiteLoad = function() {
-    const iframe = getIframe()
-
-    iframe.removeEventListener("load", onSiteLoad)
-    iframe.classList.add("loaded")
-}
-
-function loadNextSite() {
-    const iframe = getIframe()
-
-    iframe.classList.remove("loaded")
-    iframe.addEventListener("load", onSiteLoad)
-
-    iframe.src = "dat://pfrazee.hashbase.io/blog/unwalled-garden"
-}
-
-function initialize() {
-    const nextButton = document.getElementById("datscool")
-
-    nextButton.addEventListener("click", () => loadNextSite())
-}
-
-document.addEventListener("DOMContentLoaded", () => initialize())
+document.addEventListener("DOMContentLoaded", () => new App())
